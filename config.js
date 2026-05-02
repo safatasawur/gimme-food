72c8ecb37e50a66ca96eca627e901b8cc7a90f45
AbdallaZahra <abdallazahra436@gmail.com>
2026-05-02 18:27:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.API_BASE_URL = 'http://127.0.0.1:5000'; 
-// window.API_BASE_URL = 'https://gimme-food-service-production.up.railway.app';
+
+window.API_BASE_URL = 'https://gimme-food-service-production.up.railway.app';
